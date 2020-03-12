69d8481893a4482e4f126af3e8e90fdfe6631920
aaronr33-dev <aaronm.robinson97@gmail.com>
2020-03-12 09:19:56 -0500

    added a for loop to my code

--- src/main/java/testpack/Main.java
+++ src/main/java/testpack/Main.java
@@ -1,11 +1,17 @@
 package testpack;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+        /*
         byte x = 127;
         System.out.println(x);
         float myFloat = 3.12f;
         System.out.println(myFloat);
-
+        */
+        for(int p = 1; p <=5 ; p++) {
+            System.out.println(p);
+        }
     }
 }
