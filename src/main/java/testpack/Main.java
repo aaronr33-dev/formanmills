25b8d33eb9b6bfca58877fb56a857027bf69d6e4
aaronr33-dev <aaronm.robinson97@gmail.com>
2020-03-12 08:36:44 -0500

    I am a real nigga

---
+++ src/main/java/testpack/Main.java
@@ -0,0 +1,11 @@
+package testpack;
+
+public class Main {
+    public static void main(String[] args) {
+        byte x = 127;
+        System.out.println(x);
+        float myFloat = 3.12f;
+        System.out.println(myFloat);
+
+    }
+}
